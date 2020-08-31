084e4a3a549e539f031e9560dc5dfc5927cc4f63
Dennis F <dennisfj@online.no>
2020-08-31 16:47:03 +0200

    create package and classes

---
+++ src/gui/EntryPoint.java
@@ -0,0 +1,8 @@
+package gui;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		new PiFinder();
+	}
+}
---
+++ src/gui/PiFinder.java
@@ -0,0 +1,9 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class PiFinder extends JFrame {
+
+	//visualize in frame?
+	
+}
