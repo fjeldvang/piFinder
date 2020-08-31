854968ea96bfd9cdb40567d3876e567c8ea5dd2c
Dennis F <dennisfj@online.no>
2020-08-31 16:51:11 +0200

    setup frame and scanner

--- src/gui/PiFinder.java
+++ src/gui/PiFinder.java
@@ -1,9 +1,27 @@
 package gui;
 
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 
 public class PiFinder extends JFrame {
 
 	//visualize in frame?
 	
+	PiFinder(){
+		this.setSize(600, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setResizable(false);
+		getNumber();
+	}
+
+	private void getNumber() {
+		System.out.println("Enter amount of times a coordinate will be added to estimate pi");
+		
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		scanner.close();
+		
+	}
 }
