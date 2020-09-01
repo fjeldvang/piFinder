40328a25cd9069bef0664a7b56472ae3374a710c
Dennis F <dennisfj@online.no>
2020-09-01 18:02:30 +0200

    clean code and comment

--- src/gui/PiFinder.java
+++ src/gui/PiFinder.java
@@ -19,10 +19,14 @@ public class PiFinder extends JFrame {
 	PiFinder(){
 		System.out.println("***PiFinder***");
 		System.out.println("");
+		
+		//setup frame
 		this.setSize(600, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setResizable(false);
+		
+		//get number from scanner
 		getNumber();
 	}
 	
@@ -38,46 +42,52 @@ public class PiFinder extends JFrame {
 		 //int cntrX = getWidth()/2;
 		 //int cntrY = getHeight()/2;
 		 //gr.drawOval(cntrX-radius, cntrY-radius, radius*2, radius*2);
-		 //gr.drawOval(-500, -500, radius*3, radius*3);
 		 
 		 //TODO: Figure out circle outline to reflect 1x1 coordinate system
 		 
 		 if(Math.pow(intx, 2)+Math.pow(inty, 2)<=Math.pow(getWidth(), 2)) {
 			 gr.setColor(Color.green);
-		 }else {
+		 } 
+		 else {
 			 gr.setColor(Color.red);
 		 }
-		 gr.fillOval(intx, inty, radius/32, radius/32);
-		 //System.out.println(intx);
-		 //System.out.println(inty);
+		 gr.fillOval(intx, inty, radius/128, radius/128);
 	}
 
 	private void getNumber() {
-		//TODO: input via dialog instead of scanner?
+		//TODO: Input via dialog instead of scanner?
 		System.out.println("Enter amount of times a coordinate will be added to estimate pi");
+		
+		//scanner input
 		Scanner scanner = new Scanner(System.in);
 		int a = scanner.nextInt();
 		scanner.close();
-		calculatePi(a);
+		
+		//make coordinate calculations
+		calculateCoordinates(a);
+		
+		//calculate pi
 		pi = 4.0 * numPointsInCircle/numPointsTotal;
 		System.out.println("Approximation of pi by trying " + a + " times equals " + pi);
 	}
 
-	private void calculatePi(int a) {
+	private void calculateCoordinates(int a) {
 		double z = 2.0;
 		Random r = new Random();
 		
 		for(int i=0;i<a;i++) {
 			
+			//get random coordinates
 			x = r.nextDouble();
 			y = r.nextDouble();
 			
 			System.out.println("x = "+x);
 			System.out.println("y = "+y);
 			
-			distance = Math.pow(x, z) + Math.pow(y, z);
+			distance = Math.pow(Math.pow(x, z) + Math.pow(y, z),z);
 			System.out.println("Distance = "+distance);
 			
+			//find if inside circle or not
 			if(distance<=1) {
 				numPointsInCircle++;
 				System.out.println("Number inside");
